0c7e103e563124a6fb4cd4d31d879ef7a3ea63cd
halsn <xuhalsn@gmail.com>
2017-04-17 10:33:07 +0800

    auto commit

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ source $HOME/.nvm/nvm.sh
 source $HOME/.profile
 source $HOME/.bashrc
 nvm install stable
-npm install js-beautify eslint_d babel-eslint eslint-config-airbnb eslint-plugin-import eslint-plugin-react eslint-plugin-jsx-a11y htmlhint jsonlint csslint -g
+npm install js-beautify eslint_d babel-eslint eslint-config-airbnb eslint-plugin-import eslint-plugin-react eslint-plugin-jsx-a11y htmlhint eslint jsonlint csslint -g
 
 sudo pip3 install --upgrade pip
 sudo pip3 install neovim
